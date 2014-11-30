a263ae8d6e5053140f44d5075f13d8fbf3066bd0
ps67 <psaucy-67@yahoo.fr>
2014-11-30 18:19:44 +0100

    lien sur agenda sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,11 +4,11 @@
 <!-- ============ debut section ============== -->
 
 <div id="news">
-<h2>&nbsp; Agenda &nbsp;</h2>
+<a href="agenda.php" ><h2>&nbsp; Agenda &nbsp;</h2></a>
 <p><u>Dim 28 Septembre:</u><br>RASSEMBLEMENT MOTARDS<br>Restauration-Concerts-Stands multiples</p>
 
 </div>
-<a href="contact.php" ><img src="images/info.jpg"/>
+<a href="contact.php" ><img src="images/info.jpg"/></a>
 
 <div id="contact">
 <img src="images/contact.jpg" />
