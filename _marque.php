3429977688316b9f4d706929b20f5c93cf515094
ps67 <psaucy-67@yahoo.fr>
2014-11-30 11:27:09 +0100

    anciens fichiers enleves

--- _marque.php
+++
@@ -1,8 +0,0 @@
-<?php
-# affiche un texte déroulant sur la page d'accueil, sous le titre
-
-echo '
-<marquee bgcolor="#000080" style="font-size: 20" behavior="scroll" > *** RASSEMBLEMENT MOTARDS - Dimanche 28/09 *** &nbsp;&nbsp;&nbsp; Entrée gratuite: Restauration - Concerts - Stands multiples
-</marquee>';
-
-?>
